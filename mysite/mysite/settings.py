5905be468620a0294742aeb734d77b3236acd99f
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2025-04-23 14:07:51 -0400

    trying IPN from PayPal

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://160maincarryout.com', '160maincarryout.com', 'voiceaicarryout-production.up.railway.app', 'https://voiceaicarryout-production.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://160maincarryout.com', '160maincarryout.com', 'voiceaicarryout-production.up.railway.app', 'https://voiceaicarryout-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://160maincarryout.com', 'https://voiceaicarryout-production.up.railway.app']
 
 
