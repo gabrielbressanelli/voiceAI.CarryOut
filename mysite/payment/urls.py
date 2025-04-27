7b8d983fc028a920403ec01b7dccee53ee3febae
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2025-04-27 11:57:33 -0400

    fixed typo on urls.py

--- mysite/payment/urls.py
+++ mysite/payment/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('payment_success', views.payment_success, name="payment_success"),
-    path('payment_failed', views.payment_failed, name="payment_failed "),
+    path('payment_failed', views.payment_failed, name="payment_failed"),
     path('delivery', views.delivery_form, name="delivery_form"),
     path('billing_info', views.billing_info, name='billing_info'),
     path('process_order', views.process_order, name='process_order'),
