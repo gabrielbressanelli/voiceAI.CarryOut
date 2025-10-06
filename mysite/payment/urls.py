53f5c3ee98a37635850c8dd3407bc7794ba16a4f
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2025-10-06 13:29:39 -0400

    fixed payment urls

--- mysite/payment/urls.py
+++ mysite/payment/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = "payment"
+
 urlpatterns = [
     path('payment_success', views.payment_success, name="payment_success"),
     path('payment_failed', views.payment_failed, name="payment_failed"),
@@ -8,8 +10,8 @@ urlpatterns = [
     path('billing_info', views.billing_info, name='billing_info'),
     path('process_order', views.process_order, name='process_order'),
     path('paypal', include("paypal.standard.ipn.urls")),
-    path("pay/create-checkout-session/", views.create_checkout_session, name="create_checkout_session"),
-    path("pay/success/", views.checkout_success, name="checkout_success"),
-    path("pay/cancel/", views.checkout_cancel, name="checkout_cancel"),
-    path("pay/webhook/", views.stripe_webhook, name="stripe_webhook"),
+    path("create-checkout-session/", views.create_checkout_session, name="create_checkout_session"),
+    path("success/", views.checkout_success, name="checkout_success"),
+    path("cancel/", views.checkout_cancel, name="checkout_cancel"),
+    path("webhook/", views.stripe_webhook, name="stripe_webhook"),
 ]
