3cf5252c5e0ab8a4dcfd2e998313c649e809158b
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2025-10-13 12:43:26 -0400

    changed url name

--- mysite/payment/urls.py
+++ mysite/payment/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('payment_success', views.payment_success, name="payment_success"),
     path('payment_failed', views.payment_failed, name="payment_failed"),
-    path('delivery', views.delivery_form, name="delivery_form"),
+    path('delivery', views.delivery_form, name="delivery"),
     path('billing_info', views.billing_info, name='billing_info'),
     path('process_order', views.process_order, name='process_order'),
     path('paypal', include("paypal.standard.ipn.urls")),
