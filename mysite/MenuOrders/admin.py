dd82a98f5b23ce20245f25fb1e7711f1a3768253
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2025-10-13 15:46:04 -0400

    adding menu modifiers to admin page

--- mysite/MenuOrders/admin.py
+++ mysite/MenuOrders/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Menu, Cart
+from .models import Menu, Cart, ModifierGroup, ModfierOption, MenuModifierGroup
 
 admin.site.register(Menu)
 admin.site.register(Cart)
+admin.site.register(ModifierGroup)
+admin.site.register(ModfierOption)
+admin.site.register(MenuModifierGroup)
 
 
