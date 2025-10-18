4bc788d4d80be89aa746285b5f3ba7de9cd1797b
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2025-10-18 09:40:16 -0300

    added modifiergorup to admin page

--- mysite/MenuOrders/admin.py
+++ mysite/MenuOrders/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Menu, Cart, ModifierGroup, ModfierOption, MenuModifierGroup
+from .models import Menu, Cart, ModifierGroup, ModifierOption, MenuModifierGroup
 
 admin.site.register(Menu)
 admin.site.register(Cart)
 admin.site.register(ModifierGroup)
-admin.site.register(ModfierOption)
+admin.site.register(ModifierOption)
 admin.site.register(MenuModifierGroup)
 
 
