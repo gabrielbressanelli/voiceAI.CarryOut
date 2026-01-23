f8817c1424b2e88d48dba78f86c3c7b7264a3121
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2026-01-23 10:52:50 -0500

    fix token printing barear on settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,7 +33,7 @@ load_dotenv()
 
 DB_PASSWORD_YO = os.environ.get('DB_PASSWORD_YO', '')
 PRINT_SERVICE_URL = os.environ.get("PRINT_SERVICE_URL", "")
-PRINT_SERVICE_TOKEN= os.environ.get("order_printing_secret_key", "")
+PRINT_SERVICE_TOKEN= os.environ.get("PRINT_SERVICE_TOKEN", "")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
