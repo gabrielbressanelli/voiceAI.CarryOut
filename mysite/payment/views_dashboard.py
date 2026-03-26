ead3727be1721297290ad7007f2779bf18ade41b
gabrielbressanelli <gabriel.bressanelli@hotmail.com>
2026-03-26 12:31:10 -0400

    add POST endpoint for carryout order creation from Voice AI Host

--- mysite/payment/views_dashboard.py
+++ mysite/payment/views_dashboard.py
@@ -1,6 +1,10 @@
 from datetime import timedelta
 from django.shortcuts import render
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
+from django.conf import settings
 
 from payment.models import Order
 
@@ -56,3 +60,49 @@ def carryout_dashboard(request):
         "custom_date": custom_date,
     }
     return render(request, "payment/dashboard/carryout_dashboard.html", context)
+
+
+def check_bearer_token(request):
+    auth_header = request.headers.get("Authorization")
+
+    if not auth_header:
+        return False
+    try:
+        scheme, token = auth_header.split()
+    except ValueError:
+        return False
+    
+    return scheme == "Bearer" and token == settings.VOICE_ORDER_API_TOKEN
+
+@csrf_exempt
+def create_voice_AI_order(request):
+
+    if not check_bearer_token(request):
+        return JsonResponse({'message': "Unauthorized"}, status=401)
+
+    if request.method != "POST":
+        return JsonResponse({"error":"POST required"}, status=405)
+    
+    try:
+        data = json.loads(request.body)
+
+        order_ref = data.get("order_ref")
+
+        if not order_ref:
+            return JsonResponse({"error": "Missing order_ref"}, status=400)
+    
+        if Order.objects.filter(order_ref=order_ref).exists():
+            return JsonResponse({"message":"Order already exists"}, status=200)
+        
+        Order.objects.create(
+            order_ref=order_ref,
+            full_name=data.get("full_name"),
+            phone=data.get("phone"),
+            order_summary=data.get("order_summary"),
+            amount_paid=data.get("order_total", 0),
+            paid=False,
+        )
+
+        return JsonResponse({"message": "Order Created"}, status=201)
+    except Exception as e:
+        return JsonResponse({"error":str(e)}, status= 500)
